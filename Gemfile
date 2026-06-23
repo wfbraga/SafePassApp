c469f8ee2c57e706a9ae9f8691864d6df97098ef
Wagner Braga <wfbraga@hotmail.com>
2026-06-23 15:12:21 -0300

    feat: add entry sharing with colleagues via pundit authorization
    - Add Share model with belongs_to relationships to Entry and User
    - Add EntryPolicy with Pundit for authorization (show, update, destroy)
    - Implement share action to allow users to share entries with colleagues
    - Add _colleagues.html.erb partial for share UI with multi-select dropdown
    - Update entries controller to set @colleagues and @main_entry in all views
    - Add Pundit authorization include to ApplicationController
    - Add Gemfile dependency: pundit ~> 2.5
    - Update routes with POST /share endpoint
    - Seed database with multiple users for testing

--- Gemfile
+++ Gemfile
@@ -76,3 +76,5 @@ end
 gem "devise", "~> 5.0"
 
 gem "jwt", "~> 3.1"
+
+gem "pundit", "~> 2.5"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_24_231436) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_29_125950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,17 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_24_231436) do
     t.bigint "user_id", null: false
     t.string "username"
     t.index ["user_id"], name: "index_entries_on_user_id"
+  end
+
+  create_table "shares", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "entry_id", null: false
+    t.string "permission"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["entry_id", "user_id"], name: "index_shares_on_entry_id_and_user_id", unique: true
+    t.index ["entry_id"], name: "index_shares_on_entry_id"
+    t.index ["user_id"], name: "index_shares_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +49,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_24_231436) do
   end
 
   add_foreign_key "entries", "users"
+  add_foreign_key "shares", "entries"
+  add_foreign_key "shares", "users"
 end
